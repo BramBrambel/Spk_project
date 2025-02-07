da85ef159ef11a7a377d4b267227a2c55f0d358e
BramBrambel <mc8light@gmail.com>
2025-02-07 19:16:48 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
@@ -9,9 +9,7 @@ export default defineConfig({
   
 
 
-    // ...
-    output: 'server',
-    adapter: netlify(),
+   
   site: 'https://brambrambel.github.io',
 
 });
