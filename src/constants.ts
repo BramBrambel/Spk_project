e7deffe584107e6439e804cf548477e42bfdbdde
BramBrambel <mc8light@gmail.com>
2025-02-16 20:57:15 +0300

    update main page

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'SPK project company';
 export const SITE_DESCRIPTION ='Performing lighting design & creating the light concept'; 
-export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const HOMEPAGE_ARTICLE_LIMIT = 0;
 export const ARTICLES_PER_PAGE =6;
